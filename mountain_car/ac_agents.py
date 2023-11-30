CHKPT: Semi-gradient fixes things!
Also, can do multiple layer networks!

mountain_car/ac_agents.py
@@ -1,5 +1,8 @@
+from functools import partial
+
 import torch
 from torch import nn
+
 
 from torch.distributions import Categorical
 import torch.nn.functional as F
@@ -32,6 +35,20 @@ class Policy(nn.Module):
         return action_probs, value_est
 
 
+def net_from_layer_sizes(layer_sizes, activation=nn.ELU, final_activation=None):
+    layers = []
+    for ii in range(len(layer_sizes)-1):
+        layers.append(nn.Linear(layer_sizes[ii], layer_sizes[ii+1]))
+        layers.append(activation())
+
+    # Remove final activation and re-add it
+    layers.pop()
+    if final_activation is not None:
+        layers.append(final_activation())
+
+    return nn.Sequential(*layers)
+
+
 class MountainCarActorCriticAgent:
 
     def __init__(self, n_actions, agent_params={}, train_params={}, device="cpu"):
@@ -45,7 +62,11 @@ class MountainCarActorCriticAgent:
         self.n_actions = n_actions
         self.min_vals = MIN_VALS
         self.max_vals = MAX_VALS
-        self.n_hidden = agent_params.get('n_hidden', 512)
+        self.n_hidden = agent_params.get('n_hidden', None)
+
+        self.actor_layers = agent_params.get('actor_layers', [48, 48])
+        self.critic_layers = agent_params.get('critic_layers', [384, 384])
+
         self.n_state = agent_params.get('n_state', 2)
         self.mu = (torch.tensor(self.max_vals) + torch.tensor(self.min_vals))/2.0
         self.sigma = (torch.tensor(self.max_vals) - torch.tensor(self.min_vals))/2.0
@@ -110,7 +131,6 @@ class MountainCarActorCriticAgent:
         return features
 
     def step(self, reward, state, debug=False):
-        self.optimizer.zero_grad(set_to_none=True)
         if debug:
             weights = self.net.base_layer[0].weight.clone().detach()
 
@@ -122,22 +142,30 @@ class MountainCarActorCriticAgent:
         if self.single_grad:
             with torch.no_grad():
                 value_est = self.critic(features)
-            total_return_est = reward + self.gamma * value_est
         else:
             value_est = self.critic(features)
-            total_return_est = reward + self.gamma * value_est
+        total_return_est = reward + self.gamma * value_est
 
-        delta = total_return_est - prev_value_est
+        # Semi-gradient update
+        delta = total_return_est - prev_value_est.item()
 
         policy_loss = delta * -last_log_prob
         value_loss = F.smooth_l1_loss(total_return_est, prev_value_est)
         loss = policy_loss + value_loss
 
-        self.optimizer.zero_grad(set_to_none=True)
+        if self.optimizer is None:
+            self.actor_optimizer.zero_grad(set_to_none=True)
+            self.critic_optimizer.zero_grad(set_to_none=True)
+            value_loss.backward(retain_graph=True)
+            policy_loss.backward()
+            self.critic_optimizer.step()
+            self.actor_optimizer.step()
+        else:
+            self.optimizer.zero_grad(set_to_none=True)
+            loss.backward()
+            self.optimizer.step()
         # if debug:
         #     loss.retain_grad()
-        loss.backward()
-        self.optimizer.step()
 
         next_action = self.select_action(features=features)
         value_est = self.critic(features)
@@ -162,18 +190,28 @@ class MountainCarActorCriticAgent:
         return self.last_action
 
     def finish(self, reward):
-        self.optimizer.zero_grad(set_to_none=True)
+        # self.optimizer.zero_grad(set_to_none=True)
         last_log_prob = self.last_log_prob
         prev_value_est = self.last_value_est
-        delta = reward - prev_value_est
+        # Semi-gradient update
+        delta = reward - prev_value_est.item()
 
         policy_loss = delta * -last_log_prob
         value_loss = F.smooth_l1_loss(
             torch.tensor([reward], device=self.device), prev_value_est
         )
-        loss = policy_loss + value_loss
-        loss.backward()
-        self.optimizer.step()
+        if self.optimizer is None:
+            self.actor_optimizer.zero_grad(set_to_none=True)
+            self.critic_optimizer.zero_grad(set_to_none=True)
+            value_loss.backward(retain_graph=True)
+            policy_loss.backward()
+            self.critic_optimizer.step()
+            self.actor_optimizer.step()
+        else:
+            self.optimizer.zero_grad(set_to_none=True)
+            loss = policy_loss + value_loss
+            loss.backward()
+            self.optimizer.step()
 
     def reset(self):
         # self.net = Policy(
@@ -181,27 +219,51 @@ class MountainCarActorCriticAgent:
         # ).to(self.device)
         # params = self.net.parameters()
 
-        self.actor = nn.Sequential(
-            nn.Linear(self.n_state, self.n_hidden),
-            nn.ReLU(),
-            nn.Linear(self.n_hidden, self.n_actions),
-            nn.Softmax(dim=-1)
-        ).to(self.device)
-        self.critic = nn.Sequential(
-            nn.Linear(self.n_state, self.n_hidden),
-            nn.ReLU(),
-            nn.Linear(self.n_hidden, 1),
-        ).to(self.device)
-        params = list(self.actor.parameters()) + list(self.critic.parameters())
+        if self.n_hidden is not None:
+            self.actor = nn.Sequential(
+                nn.Linear(self.n_state, self.n_hidden),
+                # nn.ReLU(),
+                nn.ELU(),
+                nn.Linear(self.n_hidden, self.n_actions),
+                nn.Softmax(dim=-1)
+            ).to(self.device)
+            self.critic = nn.Sequential(
+                nn.Linear(self.n_state, self.n_hidden),
+                # nn.ReLU(),
+                nn.ELU(),
+                nn.Linear(self.n_hidden, 1),
+            ).to(self.device)
+            params = list(self.actor.parameters()) + list(self.critic.parameters())
 
+            if self.optimizer_name == "adam":
+                self.optimizer = torch.optim.AdamW(
+                    params,
+                    **self.train_params
+                )
+            else:
+                self.optimizer = torch.optim.SGD(
+                    params,
+                    **self.train_params
+                )
 
-        if self.optimizer_name == "adam":
-            self.optimizer = torch.optim.AdamW(
-                params,
-                **self.train_params
-            )
         else:
-            self.optimizer = torch.optim.SGD(
-                params,
-                **self.train_params
+            actor_layer_sizes = [self.n_state] + self.actor_layers + [self.n_actions]
+            self.actor = net_from_layer_sizes(
+                actor_layer_sizes,
+                final_activation=partial(nn.Softmax, dim=-1)
             )
+            critic_layer_sizes = [self.n_state] + self.critic_layers + [1]
+            self.critic = net_from_layer_sizes(
+                critic_layer_sizes,
+            )
+            self.optimizer = None
+            self.actor_optimizer = torch.optim.AdamW(
+                self.actor.parameters(),
+                **self.train_params['actor']
+            )
+            self.critic_optimizer = torch.optim.AdamW(
+                self.critic.parameters(),
+                **self.train_params.get('critic', {})
+            )
+
+