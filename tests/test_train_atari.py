de25aeaf3aebe18f340ac886f523afea6c4f3e24
Cory Lorenz <clorenz@gmail.com>
2023-12-31 16:39:27 -0800

    CHKPT: Different clip grads. Still not great results.

--- tests/test_train_atari.py
+++ tests/test_train_atari.py
@@ -65,7 +65,7 @@ def test_space_invaders_a3c():
         'n_actions': 6,
         'gamma': 0.99,
         'entropy_weight': 0.01,  # 0.05 saw things happening...
-        'clip_grad_norm': 5.0,
+        'clip_grad_norm': 0.3,
         'type': 'a2c-atari',
         'reward_clip': 1.0,
     }
@@ -87,8 +87,8 @@ def test_space_invaders_a3c():
     print("")  # For ease of reading
     agent, solved = a3c.train_loop_parallel(
         n_workers, agent_params, train_params, env_params,
-        log_interval=100, seed=8888101888, total_step_limit=5e9,
-        steps_per_batch=5, avg_decay=0.95, eval_interval=10,
+        log_interval=500, seed=8888101888, total_step_limit=5e9,
+        steps_per_batch=5, avg_decay=0.95, eval_interval=20,
         # out_dir=os.path.join(DEFAULT_DIR, "a3c_test")
     )
     import ipdb; ipdb.set_trace()
