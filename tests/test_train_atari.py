1ed84803b4afff64e26d54514b560aaed870a50e
Cory Lorenz <clorenz@gmail.com>
2024-01-06 14:52:06 -0800

    Fix the rendering script to use new workers as well

--- tests/test_train_atari.py
+++ tests/test_train_atari.py
@@ -25,6 +25,7 @@ def test_space_invaders_train():
         'entropy_weight': 0.01,  # 0.05 saw things happening...
         'grad_clip': 5.0,
         'type': 'a2c-atari',
+        'reward_clip': 1.0
     }
     train_params = {
         'optimizer': 'rmsprop',
@@ -33,30 +34,23 @@ def test_space_invaders_train():
     }
 
     env_name = 'ALE/SpaceInvaders-v5'
+
+    env_params = {
+        'env_name': env_name,
+        'reward_clip': 1.0,
+        'repeat_action_probability': 0.0,
+    }
     n_workers = 4
 
     torch.manual_seed(8888101)
 
-    global_agent = cor_rl.agents.factory(agent_params, train_params)
-    agents = []
-    envs = []
-    for i in range(n_workers):
-        env = environments.factory(env_name, render_mode='human' if i == 0 else None)
-        env.reset(seed=888 + i * 101)
-        envs.append(env)
-        agents.append(cor_rl.agents.factory(agent_params, train_params))
-
-    agent, solved = a3c.train_loop(
-        global_agent, agents, envs,
+    print("")
+    a3c.train_loop_parallel(
+        n_workers, agent_params, train_params, env_params,
         log_interval=10, seed=888, total_step_limit=500000,
-        steps_per_batch=5, avg_decay=0.95, max_ep_steps=1e9
+        steps_per_batch=5, avg_decay=0.95,  # max_ep_steps=1e9,
+        serial=False, render=True
     )
-    # print("")  # For ease of reading
-    # agent, solved = a3c.train_loop(
-    #     n_workers, agent_params, train_params, env_name,
-    #     log_interval=10, seed=888, total_step_limit=50000,
-    #     steps_per_batch=5, avg_decay=0.95,
-    # )
     import ipdb; ipdb.set_trace()
 
 
@@ -65,7 +59,7 @@ def test_space_invaders_a3c():
         'n_actions': 6,
         'gamma': 0.99,
         'entropy_weight': 0.01,
-        'clip_grad_norm': 0.005,
+        'clip_grad_norm': 5.0,
         'type': 'a2c-atari',
         'reward_clip': 1.0,
     }
@@ -90,8 +84,8 @@ def test_space_invaders_a3c():
         log_interval=500, seed=8888101888,
         total_step_limit=5e9,
         steps_per_batch=5, avg_decay=0.95,
-        eval_interval=0.5, save_interval=2,
         out_dir=os.path.join(DEFAULT_DIR, "a3c_test"),
-        use_mlflow=True
+        eval_interval=0.5, save_interval=2, use_mlflow=True,
+        # use_mlflow=False, serial=True
     )
     import ipdb; ipdb.set_trace()
